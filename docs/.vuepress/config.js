1a96f27a2dfafd82a1332ef4d15e5d23b81b3152
Daniel Heppner <danielhep@Daniels-MacBook-Pro.local>
2020-06-02 01:06:07 -0700

    added config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: 'Headways Transit Display',
+  description: 'Open-source project to display transit feed data from a OneBusAway API server.',
+  base: "https://departures.headwaysapp.net/"
+}
